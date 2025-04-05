885afa8ee43a31dc84921c16f2391b6d1b4b27d7
Yineth <dani.palacios@est.iudigital.edu.co>
2025-04-05 18:59:12 -0500

    agrego url bak

--- src/helper/axios-config.js
+++ src/helper/axios-config.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:4000/'
+   // baseURL: 'http://localhost:4000/'
+    baseURL: 'https://inventario-back-oxpr.onrender.com/'
+
 });
 
 export {
